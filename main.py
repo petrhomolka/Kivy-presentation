e440c13fe2b6a586a08b0ba79f34d3d3af260b5e
Petr Homolka <petrdake@gmail.com>
2014-12-23 21:48:00 +0100

    License
    Add license GNU GPL v3

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+# <This File is part of Kivy-Presentation.>
+# Copyright (C) <2014>  <Petr Homolka>
+#
+# Kivy-Presentation is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Kivy-Presentation is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# <Kivy-Presentation>  Copyright (C) <2014>  <Petr Homolka>
+#
+# Kivy-Presentation comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+# This is free software, and you are welcome to redistribute it
+# under certain conditions; type `show c' for details.
+
 from kivy.app import App
 from kivy.uix.widget import Widget 
 from kivy.properties import ObjectProperty, StringProperty
